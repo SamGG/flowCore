a444f31da1756b056761693de07f6465a87bd9ca
SamGG <samuel.granjeaud@inserm.fr>
2025-01-25 21:45:14 +0100

    Add get/set to transformList and transformMap.

--- R/transformList-accessors.R
+++ R/transformList-accessors.R
@@ -42,3 +42,81 @@ setMethod("c",
       })
 
 
+## ==========================================================================
+## Get/Set of functions of a transformList or a transformMap
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+#' Get the parameters of the function called by the transformation
+#'
+#' @param trans a transformation list (such as the value returned by
+#'   transformList or estimateLogicle functions) or a transform map (such as the
+#'   value returned by arcsinhTransform or logicleTransform functions).
+#' @returns a list of parameters  of the function called by each  transformation
+#'   of trans.
+#' @export
+transform_getParams <- function(trans)
+{
+  getParams <- function(transMap) {
+    func <- transMap@f
+    env_func <- environment(func)
+    vars <- ls(env_func)
+    vals <- sapply(vars, function(v) get(v, envir = env_func), simplify = FALSE)
+    return(vals)
+  }
+  if (inherits(trans, "transformMap"))
+    return(getParams(trans))
+  if (inherits(trans, "transformList"))
+    return(lapply(trans@transforms, getParams))
+  stop("Not implemented for an object of class ", class(trans))
+}
+
+#' Set the parameters of the function called by the transformation
+#'
+#' @param trans a transformation list (such as the value returned by
+#'   transformList or estimateLogicle functions) or a transform map (such as the
+#'   value returned by arcsinhTransform or logicleTransform functions).
+#' @param params a list of parameters as returned by transfom_getParams
+#'   function.
+#' @returns a transformation list or a single transformation map according to
+#'   trans.
+#' @export
+transform_setParams <- function(trans, params)
+{
+  setParams <- function(transMap, params_map) {
+    func <- transMap@f
+    env_func <- environment(func)
+    vars <- ls(env_func)
+    varp <- names(params_map)
+    comm <- intersect(vars, varp)
+    if (length(comm)) {
+      # make a copy of the environment to avoid overwriting
+      env_copy <- as.environment(as.list(env_func, all.names=TRUE))
+      # update the copy
+      for (v in comm) {
+        assign(v, unname(params_map[v]), envir = env_copy)
+      }
+      # assign the copy to the function
+      environment(transMap@f) <- env_copy
+    }
+    return(transMap)
+  }
+  if (inherits(trans, "transformMap")) {
+    # TODO: check params match trans
+    return(setParams(trans, params))
+  }
+  if (inherits(trans, "transformList")) {
+    # TODO: check params match trans
+    trans_maps <- trans@transforms
+    trans_mrks <- names(trans_maps)
+    params_mrks <- names(params)
+    comm_mrks <- intersect(trans_mrks, params_mrks)
+    if (length(comm_mrks)) {
+      for (mrk in comm_mrks) {
+        trans_maps[[mrk]] <- setParams(trans_maps[[mrk]], params[[mrk]])
+      }
+    }
+    trans@transforms <- trans_maps
+    return(trans)
+  }
+  stop("Not implemented for an object of class ", class(trans))
+}
